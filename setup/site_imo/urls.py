bbb90fcb7e04c68671fa0f975af52abe41a24107
Guilherme <guiassishg11@gmail.com>
2023-10-30 19:53:06 -0300

    templates funcionando

--- setup/site_imo/urls.py
+++ setup/site_imo/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name= "index")
+    path("",views.index, name= "index"),
+    path("contact",views.contact, name= "contact"),
+    path("properties",views.properties, name= "properties"),
+    path("property",views.property, name= "property-details"),
+    
+    
 ]
--- setup/site_imo/views.py
+++ setup/site_imo/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(reqquest):
-    return HttpResponse ('<h1>das</h1>')
+def index(request):
+    return render (request,'index.html')
+def contact(request):
+    return render (request,'contact.html')
+def properties(request):
+    return render (request,'properties.html')
+def property(request):
+    return render (request,'property-details.html')
