c58c5f3b056c6b51c48d0f87cbb1480bd13206f5
Guilherme <guiassishg11@gmail.com>
2023-10-30 19:24:48 -0300

    criação da url

--- setup/imobiliaria/settings.py
+++ setup/imobiliaria/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'site_imo'
+    'site_imo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
