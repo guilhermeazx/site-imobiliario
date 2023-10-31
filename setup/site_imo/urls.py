6e02d49082f1fe4490e45b7420c023560a87ded7
Guilherme <guiassishg11@gmail.com>
2023-10-30 22:27:28 -0300

    final da aula

--- setup/site_imo/urls.py
+++ setup/site_imo/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index",views.index, name= "index"),
+    path("",views.index, name= "index"),
+    path("Home",views.index, name= "index"),
     path("contact",views.contact, name= "contact"),
     path("properties",views.properties, name= "properties"),
     path("property",views.property, name= "property-details"),
--- setup/site_imo/views.py
+++ setup/site_imo/views.py
@@ -1,5 +1,9 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Produto, Vendas, Vendedor
+from datetime import datetime
+from django.db.models import Sum
+
 
 # Create your views here.
 def index(request):
@@ -12,3 +16,11 @@ def property(request):
     return render (request,'property-details.html')
 def dashboard(request):
     return render (request,'dashboard.html')
+
+
+def retorna_total_vendido(request):
+    total = Vendas.objects.all().aggregate(Sum('total'))['total__sum']
+    if request.method == "GET":
+        return JsonResponse({'total': total})
+
+
