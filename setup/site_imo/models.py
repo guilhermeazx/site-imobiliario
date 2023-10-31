f07cbeecdef9a7b13630ccf50b1e19bc7ecf227f
Guilherme <guiassishg11@gmail.com>
2023-10-30 21:52:55 -0300

    migrate feito

--- setup/site_imo/models.py
+++ setup/site_imo/models.py
@@ -1,3 +1,27 @@
+from datetime import datetime
+from typing import ClassVar
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Vendedor(models.Model):
+    nome = models.CharField(max_length=50)
+
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Vendas(models.Model):
+    nome_produto = models.ForeignKey(Produto, on_delete=models.DO_NOTHING)
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.DO_NOTHING)
+    total = models.FloatField()
+    data = models.DateTimeField(default=datetime.now())
+
+
+    def __str__(self):
+        return self.nome_produto.nome
