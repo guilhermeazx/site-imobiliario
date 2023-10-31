103896fe131c9b2cbcb886149b98b79478433822
Guilherme <guiassishg11@gmail.com>
2023-10-30 21:26:02 -0300

    partials/footer feito

--- setup/site_imo/urls.py
+++ setup/site_imo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name= "index"),
+    path("index",views.index, name= "index"),
     path("contact",views.contact, name= "contact"),
     path("properties",views.properties, name= "properties"),
     path("property",views.property, name= "property-details"),
